c5389cbfd80cfd379b71b85d6657e5971c62319b
Ruandev2 <93599287+Ruandev2@users.noreply.github.com>
2024-01-16 18:17:00 -0300

    configuracao Github Pages1

--- 10_MINI_BLOG/miniblog/src/components/Footer.js
+++ 10_MINI_BLOG/miniblog/src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
         <h3>Escreva o que voce tem interesse !</h3>
-        <p>Mini Blog &copy;2023</p>
+        <p>Mini Blog &copy;2024</p>
     </footer>
   )
 }
